1094accff840842107eb1869f441602904ad5787
Abdelhak-Chellal <abdelhak.chellal@ensia.edu.dz>
2023-12-08 10:24:54 +0100

    adding the endpoints to comminicate with gpt

--- gpt_integration/views.py
+++ gpt_integration/views.py
@@ -1,19 +1,70 @@
-
 # Create your views here.
 from django.http import JsonResponse
 from django.views import View
 import openai
 
 
-class GPT3PromptView(View):
+class GptTellStory(View):
     def get(self, request, *args, **kwargs):
-        # prompt = request.POST.get('prompt', '')
-        prompt = "who is Lala Fatma nsoumer"
+        prompt = "make me a filling gap exercice"
 
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-1106",  # Use the appropriate GPT-3 fine tuned model
+            model="gpt-3.5-turbo-1106",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
+
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        generated_text = response['choices'][0]['message']
+
+        return JsonResponse({'generated_text': generated_text})
+
+
+class GptQuiz(View):
+    def get(self, request, *args, **kwargs):
+        prompt = "make me a filling gap exercice"
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        generated_text = response['choices'][0]['message']
+
+        return JsonResponse({'generated_text': generated_text})
+
+
+class GptFillGaps(View):
+    def get(self, request, *args, **kwargs):
+        prompt = "make me a filling gap exercice"
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        generated_text = response['choices'][0]['message']
+
+        return JsonResponse({'generated_text': generated_text})
+
+
+class GptGradeEssay(View):
+    def get(self, request, *args, **kwargs):
+        prompt = request.POST.get('prompt', '')
+        if not prompt:
+            return JsonResponse({'error': 'Prompt is required in the POST request'}, status=400)
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-1106",
+            messages=[
+
                 {"role": "user", "content": prompt},
             ],
         )
